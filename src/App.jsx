92836a04ce5ec7a0bae7982754480522851a6437
jarkurghan <jarkurghan@gmail.com>
2025-01-28 14:23:02 +0500

    create react/js/vite/tailwind app

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+    return (
+        <div>
+            <h1>Najmiddin Nazirov</h1>
+            <p>Developer</p>
+        </div>
+    );
+}
+
+export default App;
