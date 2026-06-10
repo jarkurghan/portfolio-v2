825b86c7c737bffefd9a3a472d8d1af99ef4858a
Najmiddin Nazirov <nnazirov@transoxiania.com>
2026-06-10 12:00:03 +0100

    remove unnecessary leading class from paragraph in Home component

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ export default function Home() {
             <main className="flex flex-1 w-full max-w-3xl flex-col items-center justify-between py-32 px-16 bg-white dark:bg-black sm:items-start">
                 <div className="flex flex-col items-center gap-6 text-center sm:items-start sm:text-left">
                     <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">Tez orada...</h1>
-                    <p className="max-w-md text-lg leading-8 text-zinc-600 dark:text-zinc-400">
+                    <p className="max-w-md text-lg text-zinc-600 dark:text-zinc-400">
                         Endi multfilmlarni ko'rib, saralab, ular haqida ma'lumot olib tanlashingiz mumkin!
                     </p>
                 </div>
