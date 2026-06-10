73dae5fd52e2a35571a0b7101813afbd95f98602
Najmiddin Nazirov <nnazirov@transoxiania.com>
2026-06-10 18:20:24 +0100

    add suppressHydrationWarning attribute to RootLayout HTML tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
     children: React.ReactNode;
 }>) {
     return (
-        <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}>
+        <html lang="en" className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`} suppressHydrationWarning>
             <head>
                 <Script src="https://telegram.org/js/telegram-web-app.js" strategy="beforeInteractive" />
             </head>
